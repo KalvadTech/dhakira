a20679c5f377be70b76174502ab6c107496faaa7
Loïc Tosser <loic@kalvad.com>
2022-02-21 15:19:54 +0400

    Fix a lot

---
+++ src/dhakira.cr
@@ -0,0 +1,8 @@
+# TODO: Write documentation for `Dhakira`
+module Dhakira
+  VERSION = "0.1.0"
+  require "totem"
+  # Load yaml file from file with path
+  totem = Totem.from_file "./dhakira.yaml"
+  # TODO: Put your code here
+end
